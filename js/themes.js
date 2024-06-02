416bfa4f225db7af3058782d31bfe6f475438f1b
stayinvest-for-dummy <waihunglo@gmail.com>
2024-06-02 17:43:27 +0800

    themes

--- js/themes.js
+++ js/themes.js
@@ -4,17 +4,24 @@ var getObjectByValue = function (array, key, value) {
     });
 };
 
-function appendThemesLink(data, key, parent) {
-    var themes = getObjectByValue(data.themes, "name", "DOW")[0];
+function appendThemesLink(data, key, linkDesc, hrefAddr, parent) {
+    // retrieve data from json
+    var themes = getObjectByValue(data.themes, "name", key)[0];
+    const tempChartLink = hrefAddr.replace(/{stockCodes}/i, themes.holdings.join());
+
+    // create link element
     var linkElement = document.createElement('a');
-    linkElement.href = "snapshots-aa.html?t=US&o=" + themes.holdings.join();
+    linkElement.href = tempChartLink;
     linkElement.setAttribute("target", "_blank");
-    linkElement.text = themes.name;
+    linkElement.text = linkDesc;
+
+    // append
     parent.appendChild(linkElement);
 }
 
-function fetchJSONData() {
-    fetch("data/sample.json")
+function fetchJSONData(key, linkDesc, hrefAddr, parent) {
+    const jsonDataFile = "data/equity-holdings.json"
+    fetch(jsonDataFile)
         .then((res) => {
             if (!res.ok) {
                 throw new Error
@@ -24,7 +31,7 @@ function fetchJSONData() {
         })
         .then((data) => {
             console.log(data);
-            appendThemesLink(data, "DOW", img_home);
+            appendThemesLink(data, key, linkDesc, hrefAddr, parent);
         })
         .catch((error) => {
             console.error("Unable to fetch data:", error);
