9d8aa393fe7183aca9f82ddf3b4cfc1c6d7bd187
stayinvest-for-dummy <waihunglo@gmail.com>
2024-06-10 14:50:49 +0800

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -118,6 +118,7 @@ const fetchStockCodesSortBy = async (stockCodes, taIndicator) => {
 }
 
 function unicodeToChar(text) {
+    text = encodeURIComponent(text);
     return text.replace(/\\u[\dA-F]{4}/gi,
         function (match) {
             return String.fromCharCode(parseInt(match.replace(/\\u/g, ''), 16));
