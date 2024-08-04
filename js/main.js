bdc9423cdd1461c60d55d1bdf4770ba887331743
stayinvest-for-dummy <waihunglo@gmail.com>
2024-08-04 08:40:57 +0800

    refactor javascript

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { resolveChartLink } from "js/utils.js";
+
+const link = resolveChartLink("0941.HK","EQUITY");
+
+console.log(link);
---
+++ js/utils.js
@@ -0,0 +1,18 @@
+export function resolveChartLink(stockCode, universe) {
+    var refLink = "https://www.stockfisher.com.hk/us-stock/ticker/{stockCode}";
+
+    if (stockCode.includes(".HK")) {
+        refLink = "https://www.stockfisher.com.hk/ticker/{stockCode}";
+    } else if ("ETF" == universe || "etf" == universe || stockCode.startsWith("^")) {
+        refLink = "https://www.tradingview.com/chart/?symbol={stockCode}";
+        stockCode = stockCode.replace("^","");
+    } else if (stockCode.startsWith("$")) {
+        refLink = "https://stockcharts.com/sc3/ui/?s={stockCode}";
+    }
+
+    const tempRefLink = refLink
+        .replace(/{stockCode}/i, stockCode);
+
+    console.log(tempRefLink);
+    return tempRefLink;
+}
