c7e9a6521268b7b2febb0f31c36a61cc406c6329
stayinvest-for-dummy <waihunglo@gmail.com>
2024-08-04 09:57:09 +0800

    Update themes.js

--- js/themes.js
+++ js/themes.js
@@ -1,71 +1,7 @@
-let emojiIdx = 0;
-const emojiTheme = 
-   ["🚃", "🚀", "💛", "❗️", "📈", "📏", "🔂", "🐓", "📇", "🐁", "💝", "🚸", "🔮", "🕖", "🗽", 
-    "🕞", "📞", "💋", "💫", "🚡", "🖨", "✏️", "🈷️", "🎱", "🙉", "🚪", "💾", "↗️", "🔣", "🚋", 
-    "💷", "🕹", "🧀", "🛅", "😱", "🌟", "🌖", "📘", "🎐", "🍏", "📙", "👷", "⬆️", "🏗", "🍕", 
-    "🎊", "🎀", "🕉", "🌏", "🐊", "🍪", "9️⃣", "🚝", "🎆", "🌦", "📟", "🍌", "☁️", "😩", "📂", 
-    "🍋", "💟", "☕️", "🎓", "🕌", "🙎", "🆙", "🔟", "😋", "🔛", "🏍", "🔓", "👱", "👡", "c", 
-    "😒", "⬛️", "🚈", "📝", "🔱", "👽", "💆", "🎒", "🔎", "⛹", "🎚", "🎦", "*⃣", "🚣", "🏓", 
-    "🤐", "🎤", "🐷", "📨", "🔜", "📑", "📛", "🔕", "😉", "🚉", "↔️", "❌", "▶️", "✝", "🍘", 
-    "🦁", "✍", "📴", "🍜", "💢", "🕛", "‼️", "💜", "🐡", "💕", "🍚", "📺", "🎥", "🖲", "⛔️", 
-    "📜", "💽", "☸", "👇", "🚾", "↩️", "🚞", "👆", "🐗", "🚨", "👊", "🐅", "🌗", "🏘", "😴", "🎅", 
-    "👲", "🌭", "🤗", "📻", "🌇", "📣", "🔻", "🌲", "🕸", "🍠", "🚤", "🙏", "🚑", "😃", "👏", "🍄", 
-    "🎻", "🛥", "➿", "➰", "6️⃣", "❕", "🍫", "✂️", "🎲", "↕️", "🐸", "🏐", "😵", "🍁", "☄", "👎", "🎷", 
-    "♑️", "2️⃣", "🚲", "🚐", "🌑", "📍", "🕵", "🚳", "👝", "⏭", "🔍", "🎺", "▫️", "↪️", "🛍", "🌡", "🎌", 
-    "🈁", "🕚", "🍯", "👴", "🏔", "😿", "🐃", "🏷"];
-
-// stock chart config
-const scConf2m = {
-    chartWidth: 305,
-    chartHeight: 225,
-    period: "dg"
-    };
-
-const scConf6m = {
-    chartWidth: 350,
-    chartHeight: 259,
-    period: "dc"
-    };
-        
-function emoji() {
-    if(emojiIdx >= emojiTheme.length) {
-        emojiIdx = 0;
-    }
-    return emojiTheme[emojiIdx++];
-}
 
 var getObjectByValue = function (array, key, value) {
     return array.filter(function (object) {
         return object[key] === value;
     });
 };
-
-function isEmpty(value) {
-    return (
-        value === null || value === undefined || value === '' ||
-        (Array.isArray(value) && value.length === 0) ||
-        (!(value instanceof Date) && typeof value === 'object' && Object.keys(value).length === 0)
-    );
-}
-
-
-
-
-
-/**
- * load stock codes image as input codes list
- */
-const loadStockCodesImageWithProgressBar = (inputStockCodes, taIndicator, imageHome, ldBarName, dataScanType, shouldSort) => {
-    partitionStockCodesAndSort(inputStockCodes, taIndicator, ldBarName, dataScanType, shouldSort)
-    .then(function (sortedStockCodes) {
-        sortedStockCodes.forEach(stockCode => {
-            var desc = stockCode.industry + "|" + stockCode.sector + "|" + stockCode.name;
-            var borderStyle = null;
-            appendImageToParent(imageHome, chartType, stockCode.symbol, stockCode.universe, desc, taIndicator, borderStyle);
-        });
-    })
-    .catch(function (error) {
-        console.error(error);
-    });		    
-} 
 
