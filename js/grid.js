5e44da36aacb4e39feb97895f327e4c5457b16ee
stayinvest-for-dummy <waihunglo@gmail.com>
2024-08-04 08:11:20 +0800

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -66,7 +66,10 @@ function fetchInGrid(parentId, stockCodes, taIndicator) {
 
 function gotoPage(stockCode, universe) {
     var refLink = "https://www.stockfisher.com.hk/us-stock/ticker/{stockCode}";
-    if ("ETF" == universe || "etf" == universe || stockCode.startsWith("^")) {
+
+    if (stockCode.includes(".HK")) {
+        refLink = "https://www.stockfisher.com.hk/ticker/{stockCode}";
+    } else if ("ETF" == universe || "etf" == universe || stockCode.startsWith("^")) {
         refLink = "https://www.tradingview.com/chart/?symbol={stockCode}";
         stockCode = stockCode.replace("^","");
     } else if (stockCode.startsWith("$")) {
