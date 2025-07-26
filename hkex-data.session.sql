938cb6f2cbc3709f3104441d88cbdb3dcd59140c
stayinvest-for-dummy <waihunglo@gmail.com>
2025-07-26 22:09:18 +0800

    fix: update FTR link for improved filtering options

---
+++ hkex-data.session.sql
@@ -0,0 +1,38 @@
+select 
+        dt,
+        case sector
+        when 'Basic Materials' then 'XLB'
+        when 'Communication Services' then 'XLC'
+        when 'Consumer Cyclical' then 'XLY'
+        when 'Consumer Cyclical' then 'XLY'
+        when 'Consumer Defensive' then 'XLP'
+        when 'Energy' then 'XLE'
+        when 'Financial Services' then 'XLF'
+        when 'Healthcare' then 'XLV'
+        when 'Industrials' then 'XLI'
+        when 'Real Estate' then 'XLF'
+        when 'Technology' then 'XLK'
+        when 'Utilities' then 'XLU'   
+        else 'XLX'
+        end sector,
+        round(cast(up4pct1d as float) / tot * 100.0, 2) u4sm, 
+        round(cast(dn4pct1d as float) / tot * 100.0, 2) d4sm,
+        round(cast(up0pct1d as float) / tot * 100.0, 2) u0sm,
+        round(cast(dn0pct1d as float) / tot * 100.0, 2) d0sm
+        FROM
+        (
+            select 
+                stock.sector, DAILY_STOCK_STATS.dt,
+                sum(case when chg_pct_1d >= 4 then 1 else 0 end)  up4pct1d  , 
+                sum(case when chg_pct_1d<= -4 then 1 else 0 end)  dn4pct1d ,
+                sum(case when chg_pct_1d > 0  then 1 else 0 end)  up0pct1d ,
+                sum(case when chg_pct_1d < 0 then 1 else 0 end)  dn0pct1d ,
+                count(1) tot
+            from stock, DAILY_STOCK_STATS
+            where stock.symbol = DAILY_STOCK_STATS.symbol
+            and sector != 'UNKNOWN'
+            group by stock.sector, DAILY_STOCK_STATS.dt
+            order by sector
+        ) order by dt desc, sector
+
+select * from stock        
